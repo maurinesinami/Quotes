da5f5a6de33bad72c73e3120c337a3ef21305080
maurinenami <maurinenami>
2019-04-28 18:24:19 +0300

    add like and dislike buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     
-    QuoteFormComponent
+    QuoteFormComponent,
+    
+    QuoteDetailsComponent
 
   ],
   imports: [
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,13 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input, EventEmitter, Output } from '@angular/core';
+import { Quote } from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  @Input() quote:Quote
+  @Output() like= new EventEmitter<boolean>();
+    // toogleDetails(index){
+    //   this.quotes[index].showQuote = !this.quotes[index].showQuote;
+  
+  
+    
+    
+  // }
+  upvote:number=0;
+  downvote:number=0;
+  thumbsUp(){
+    this.upvote++;
+  }
+  
+  thumbsDown(){
+    this.downvote++;
+  }
 
-  constructor() { }
 
   ngOnInit() {
   }
